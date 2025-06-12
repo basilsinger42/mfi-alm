f4a5a403da59c95aab0e2efadfe124074a71cc3c
Basil Singer <bksinger01@gmail.com>
2025-06-12 16:11:04 -0400

    Just a test.

--- src/mfi_alm/assets/asset.py
+++ src/mfi_alm/assets/asset.py
@@ -1,10 +1,11 @@
+from dataclasses import dataclass
 """
 TODO:
 Create a general asset class that contains the following attributes:
 price, cusip. Can leverage info from placeholder function.ipynb.
 @dataclass
 class Asset:
-    price: float
+    market_price: float
     growth_rate: float
 
     def evolve_end_of_month(self, scenario: dict):
@@ -25,5 +26,19 @@ scale
 sell
 buy
 """
+@dataclass
 class Asset:
-    pass
+    market_price: float
+    growth_rate: float
+
+    def evolve_end_of_month(self, scenario: dict):
+        # Placeholder logic (e.g. apply growth)
+        pass
+
+    def scale(self, reserve: float):
+        # Placeholder logic
+        pass
+
+    def sell(self, expense: float):
+        # Placeholder logic
+        pass
