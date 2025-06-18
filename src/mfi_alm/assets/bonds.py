2c6124c1d774dce746dea67add90467f02890ca3
Basil Singer <bksinger01@gmail.com>
2025-06-18 16:30:42 -0400

    Tweaks in code style. All good!

--- src/mfi_alm/assets/bonds.py
+++ src/mfi_alm/assets/bonds.py
@@ -1,53 +1,32 @@
-
-from mfi_alm.assets.asset import Asset
-
-"""
-TODO:
-
-Create a class called FixedBond (e.g., leverage asset_type_pricing.ipynb logic). It should inherit attributes
-from the general Asset class.
-"""
-
-import pandas as pd
-
 import numpy as np
-
-from typing import List, Tuple
 
 
 class FixedBond:
-    """ Vanilla non-callable fixed rate bonds """
-
+    """ Vanilla non-callable fixed rate bonds."""
     def __init__(self, face: float, coupon: float, maturity: float, freq: int = 2):
-        """
-        parameters:
-            face: Face value
-            coupon: Annual coupon rate
-            maturity: Maturity
-            freq: Coupon payment frequency
-        """
         self.face = face
         self.coupon = coupon
         self.maturity = maturity
         self.freq = freq
 
-    def cashflows(self) -> List[Tuple[float, float]]:
-        """Generate all future cash flows from bonds (time, amount)"""
-        # Calculate the amount of each interest payment
+    def cashflows(self) -> list[tuple[float, float]]:
+        """Generate all future cash flows from bonds (time, amount)."""
         c = self.coupon * self.face / self.freq
-        # Calculate total number of interest payments
         n = int(self.maturity * self.freq)
-        # Generate cash flow (excluding principal) for each period
+
+        # Generate cash flow (excluding principal) for each period.
         flows = [(i / self.freq, c) for i in range(1, n + 1)]
-        # Final installment plus principal
+
+        # Final installment plus principal.
         flows[-1] = (flows[-1][0], flows[-1][1] + self.face)
+
         return flows
 
     def price(self, ytm: float) -> float:
-        """Calculate bond price by continuously compounding"""
+        """Calculate bond price by continuously compounding."""
         return sum(cf * self._disc(ytm, t) for t, cf in self.cashflows())
 
     @staticmethod
     def _disc(rate: float, t: float) -> float:
-        """Continuously compounding discount factor"""
+        """Continuously compounding discount factor."""
         return np.exp(-rate * t)
--- tests/unit/test_fixed_bonds.py
+++ tests/unit/test_fixed_bonds.py
@@ -1,44 +1,33 @@
-"""
-TODO: Create unit tests that test bonds.
-"""
+import numpy as np
+import pytest
 
 from mfi_alm.assets import FixedBond
 
-import pytest
-import numpy as np
 
 @pytest.fixture
 def sample_bond() -> FixedBond:
-    """Example of a test bond"""
-    return FixedBond(
-        face=1000,
-        coupon=0.05,
-        maturity=5,
-        freq=2
-    )
+    return FixedBond(face=1000, coupon=0.05, maturity=5, freq=2)
+
 
 def test_cashflows(sample_bond: FixedBond):
-    """Test cashflow generation"""
     flows = sample_bond.cashflows()
-    #It should generate 10 periods cashflow
+
     assert len(flows) == 10
-    # First period cashflow should be 25 (0.5year, 25dollar)
     assert flows[0] == (0.5, 25)
-    # Last period cashflow should be 1025 (5year, 1025dollar)
     assert flows[-1] == (5.0, 1025)
 
+
 def test_price_at_par(sample_bond: FixedBond):
-    """Testing parity bond pricing (YTM = coupon rate)"""
     price = sample_bond.price(ytm=0.05)
-    # Prices should be close to face value (1% error allowed)
     assert np.isclose(price, 1000, rtol=1e-2)
 
+
 def test_price_above_par(sample_bond: FixedBond):
-    """Testing premium bond pricing (YTM<coupon rate)"""
+
     price = sample_bond.price(ytm=0.04)
-    assert price > 1000  # Price should be higher than face value
+    assert price > 1000
+
 
 def test_price_below_par(sample_bond: FixedBond):
-    """Testing discount bond pricing (YTM > coupon rate)"""
     price = sample_bond.price(ytm=0.06)
-    assert price < 1000  # Price should be lower than face value
+    assert price < 1000
