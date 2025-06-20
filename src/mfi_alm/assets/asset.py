8e0ca0b3862d27e2a2994700ee6b7d0a62459cc9
Guohao-web <guohao.tang@mail.utoronto.ca>
2025-06-19 22:57:48 -0400

    First version of age method for FixedBond class and a new asset class

--- src/mfi_alm/assets/asset.py
+++ src/mfi_alm/assets/asset.py
@@ -26,6 +26,8 @@ scale
 sell
 buy
 """
+
+"""
 @dataclass
 class Asset:
     market_price: float
@@ -42,3 +44,16 @@ class Asset:
     def sell(self, expense: float):
         # Placeholder logic
         pass
+"""
+
+from .bonds import FixedBond
+
+
+class Asset:
+    """An asset class representing a fixed-rate bond and its market value."""
+    def __init__(self, fixed_bond: FixedBond, ytm: float):
+        self.fixed_bond = fixed_bond
+        self.ytm = ytm
+
+    def market_value(self) -> float:
+        return self.fixed_bond.price(ytm=self.ytm)
